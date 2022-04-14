6bbbcfee0c1fff41a11b262b738becb6589c34a8
Joe King <joe.k659@gmail.com>
2022-04-14 15:51:31 -0500

    rails api

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :notes
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
